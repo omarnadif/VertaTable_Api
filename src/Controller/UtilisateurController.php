30f816c3189b0abef6965c8ee512de8ccd938bf7
Omar.nadif <Omar.nadif2022@campus-eni.fr>
2024-06-01 20:25:25 +0200

    Name home

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -12,32 +12,35 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-#[Route('/api/utilisateur')]
+#[Route('/api/user')]
 class UtilisateurController extends AbstractController
 {
     private EntityManagerInterface $em;
     private UtilisateurRepository $utilisateurRepository;
     private SerializerInterface $serializer;
     private ValidatorInterface $validator;
+    private UserPasswordHasherInterface $passwordHasher;
 
     public function __construct(
         EntityManagerInterface $em,
         UtilisateurRepository $utilisateurRepository,
         SerializerInterface $serializer,
-        ValidatorInterface $validator
+        ValidatorInterface $validator,
+        UserPasswordHasherInterface $passwordHasher
     ) {
         $this->em = $em;
         $this->utilisateurRepository = $utilisateurRepository;
         $this->serializer = $serializer;
         $this->validator = $validator;
+        $this->passwordHasher = $passwordHasher;
     }
 
     /**
      * Retourne la liste de tous les utilisateurs.
      */
     #[Route('/', name: 'utilisateur_index', methods: ['GET'])]
-
     public function index(): JsonResponse
     {
         $utilisateurs = $this->utilisateurRepository->findAll();
@@ -50,7 +53,6 @@ class UtilisateurController extends AbstractController
      * Crée un nouvel utilisateur.
      */
     #[Route('/', name: 'utilisateur_new', methods: ['POST'])]
-
     public function new(Request $request): JsonResponse
     {
         $utilisateur = $this->serializer->deserialize($request->getContent(), Utilisateur::class, 'json');
@@ -60,6 +62,10 @@ class UtilisateurController extends AbstractController
         if (count($errors) > 0) {
             return $this->json($errors, Response::HTTP_BAD_REQUEST);
         }
+
+        $utilisateur->setPassword(
+            $this->passwordHasher->hashPassword($utilisateur, $utilisateur->getPassword())
+        );
 
         $this->em->persist($utilisateur);
         $this->em->flush();
@@ -71,7 +77,6 @@ class UtilisateurController extends AbstractController
      * Affiche un utilisateur spécifique.
      */
     #[Route('/{id}', name: 'utilisateur_show', methods: ['GET'])]
-
     public function show(Utilisateur $utilisateur): JsonResponse
     {
         $data = $this->serializer->serialize($utilisateur, 'json');
@@ -83,7 +88,6 @@ class UtilisateurController extends AbstractController
      * Met à jour un utilisateur spécifique.
      */
     #[Route('/{id}', name: 'utilisateur_edit', methods: ['PUT'])]
-
     public function edit(Request $request, Utilisateur $utilisateur): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -95,6 +99,12 @@ class UtilisateurController extends AbstractController
             return $this->json($errors, Response::HTTP_BAD_REQUEST);
         }
 
+        if (isset($data['password'])) {
+            $utilisateur->setPassword(
+                $this->passwordHasher->hashPassword($utilisateur, $data['password'])
+            );
+        }
+
         $this->em->flush();
 
         return $this->json($utilisateur, Response::HTTP_OK);
@@ -104,12 +114,33 @@ class UtilisateurController extends AbstractController
      * Supprime un utilisateur spécifique.
      */
     #[Route('/{id}', name: 'utilisateur_delete', methods: ['DELETE'])]
-
     public function delete(Utilisateur $utilisateur): JsonResponse
     {
         $this->em->remove($utilisateur);
         $this->em->flush();
 
         return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Retourne les détails d'un utilisateur spécifique.
+     */
+    #[Route('/{id}/details', name: 'utilisateur_details', methods: ['GET'])]
+    public function getUserDetails(int $id): JsonResponse
+    {
+        $user = $this->utilisateurRepository->find($id);
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $userData = [
+            'id' => $user->getId(),
+            'prenom' => $user->getPrenom(),
+            'nom' => $user->getNom(),
+            'email' => $user->getEmail(),
+        ];
+
+        return new JsonResponse($userData, Response::HTTP_OK);
     }
 }
